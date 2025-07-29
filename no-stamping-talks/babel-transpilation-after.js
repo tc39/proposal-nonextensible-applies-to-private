f495f8b20a38ae58558ff8dc94cacb41816043ef
Mark S. Miller <erights@gmail.com>
2025-07-29 09:11:32 -0700

    better babel

--- no-stamping-talks/babel-transpilation-after.js
+++ no-stamping-talks/babel-transpilation-after.js
@@ -1,9 +1,14 @@
-export class C {
-  static __last_static_block__ = () => {
-    delete C.__last_static_block__;
-    Object.freeze(C);
-  };
+var lastBlocks;
+class C {
   static #FOO = "#FOO";
-  static #BAR = (() => (C.#FOO = #BAR in C, C.#FOO))();
+  static #BAR = (
+    (() => {
+      C.#FOO = #BAR in C;
+    })(),
+    lastBlocks = (() => {
+      Object.freeze(C);
+    }),
+    C.#FOO
+  );
 }
-C.__last_static_block__();
+lastBlocks();
