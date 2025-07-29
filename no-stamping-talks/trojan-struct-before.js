273541a982cd00f310ebfe388567c032d1b602d0
Mark S. Miller <erights@gmail.com>
2025-07-29 09:20:25 -0700

    trojan spacing

--- no-stamping-talks/trojan-struct-before.js
+++ no-stamping-talks/trojan-struct-before.js
@@ -1,15 +1,12 @@
 class Trojan {
   constructor(key) { return key; }
 }
-
 class Tagger extends Trojan {
   #value;
-
   constructor(key, value) {
     super(key);
     this.#value = value;
   }
-
   static getOrThrow(obj) {
     return obj.#value;
   }
